945f3a2804d65d71c62825cd41ffa16ef8662c73
Antonia Stevens <a@antevens.com>
2019-12-07 00:54:44 -0500

    Fix comments and docstrings

--- dromedary.py
+++ dromedary.py
@@ -1,15 +1,12 @@
 # Velodrome marking/line sensor
 import sensor, image, time, lcd, pyb, micropython, ulab
-
 class VeloTimer():
     """
     Primary class to find lines and send signals
-
     See documentation for the imaging sensor:
     https://github.com/antevens/dromedary/blob/master/documents/MT9V034.pdf
     """
     def __init__(self, draw_stats=False, draw_lines=False):
-
         # Setup hardware
         self.red_led = pyb.LED(1)
         self.green_led = pyb.LED(2)
@@ -20,40 +17,36 @@ class VeloTimer():
         # Set line finding parameters
         self.min_degree = 45
         self.max_degree = 135
-        self.threshold = 1000 #2000
-        self.theta_margin = 25 #100 #25
-        self.rho_margin = 25 #100 #25
+        self.threshold = 1000
+        self.theta_margin = 25
+        self.rho_margin = 25
         self.x_stride = 2
         self.y_stride = 8
 
-
         # Delta value in pixels for x/y for tracking new lines
         self.line_id_max_delta = 20
-
         # Max frames without a line before line history is cleared
         self.frames_before_line_purge = 200
 
         # Configure the imaging sensor
-        sensor.reset() # Initialize the sensor
+        sensor.reset() Initialize the sensor
         sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format
-        sensor.set_framesize(sensor.QQQVGA)  # Set frame size
+        sensor.set_framesize(sensor.QQQVGA) # Set frame size
         sensor.set_auto_exposure(True, exposure_us=5000) # Smaller means faster
-        sensor.skip_frames(time = 2000)     # Wait for settings take effect
+        sensor.skip_frames(time = 2000) # Wait for settings take effect
 
         # Configure clock for tracking FPS
         self.clock = time.clock()
-
         # Configure the lcd screen.
         lcd.init()
-
         # Initialize image buffer
         self.img = sensor.snapshot()
-
-        # Schedule async LCD shield updates
         micropython.alloc_emergency_exception_buf(100) # Debugging only
-        self.render_ref = self.render # Allocation occurs here
-        self.tim = pyb.Timer(4) # Assign timer 5
-        self.tim.init(freq=10) # Set LCD shield update freq. to 20hz
+
+        # Allocation for interrupt callbacks, async LCD redering
+        self.render_ref = self.render
+        self.tim = pyb.Timer(4)
+        self.tim.init(freq=10)
         self.tim.callback(self.cb)
 
         # Show performance/debug statistics/info
@@ -61,10 +54,7 @@ class VeloTimer():
         self.draw_lines = draw_lines
         self.line_draw_color = (255, 0, 0)
         self._fps = None
-
-        # Store known lines and set lifetime in frames
         self._known_lines = []
-
 
     def __del__(self):
         """ Clean up timers """
@@ -76,9 +66,10 @@ class VeloTimer():
             img.draw_string(0,0, "{:03.0f}fps".format(self._fps))
         return img
 
-    def draw_exposure(self, img=None):
+    def draw_exposure(self, img=None, scale=None):
          img = img or self.img
-         img.draw_string(0,50, "{:03d}el".format(self.get_exposure_lines()))
+         scale = scale or self.scale
+         img.draw_string(0,sensor.height() * scale - 10, "{:03d}el".format(self.get_exposure_lines()))
          return img
 
     def cb(self, timer):
@@ -92,17 +83,19 @@ class VeloTimer():
         self._fps = self.clock.fps()
         return self.img
 
-    def render(self, timer, img=None):
+    def render(self, timer, img=None, scale=None):
         """ Renders an image to the LCD shield """
         img = img or self.img
+        scale = scale or self.scale
         if self.draw_stats:
-            img = img.to_rgb565(copy=True)
+            img = img.copy((0,0,sensor.width(),sensor.height()),scale,scale).to_rgb565(copy=True)
             self.draw_fps(img)
             self.draw_exposure(img)
-            #print(self._fps, self.get_exposure_lines())
         if self.draw_lines:
             for iterations, line in self._known_lines:
-                img.draw_line(line.line(), color = self.line_draw_color)
+                scaled_line = ulab.array(line.line())* scale
+                print(scaled_line)
+                img.draw_line(scaled_line[0], color = self.line_draw_color)
         lcd.display(img)
 
     def get_exposure_lines(self):
@@ -110,7 +103,6 @@ class VeloTimer():
         The exposure measured in row-time from 1 to 2047.
         """
         return sensor.__read_reg(0xBB)
-
 
     def get_gain(self):
         """
@@ -123,25 +115,18 @@ class VeloTimer():
     def find_lines(self, img=None, min_degree=None, max_degree=None):
         """
         Finds lines in images
-
         `threshold` controls how many lines in the image are found. Only lines with
         edge difference magnitude sums greater than `threshold` are detected...
-
         More about `threshold` - each pixel in the image contributes a magnitude value
         to a line. The sum of all contributions is the magintude for that line. Then
         when lines are merged their magnitudes are added togheter. Note that `threshold`
         filters out lines with low magnitudes before merging. To see the magnitude of
         un-merged lines set `theta_margin` and `rho_margin` to 0...
-
         `theta_margin` and `rho_margin` control merging similar lines. If two lines
         theta and rho value differences are less than the margins then they are merged.
-
-
         All line objects have a `theta()` method to get their rotation angle in degrees.
         You can filter lines based on their rotation angle.
-
         """
-
         img = img or self.img
         lines_present = []
         for line in img.find_lines(threshold=self.threshold,
@@ -154,10 +139,11 @@ class VeloTimer():
                 continue
             if self.max_degree and line.theta() > self.max_degree:
                 continue
-            lines_present.append(line)
-            self.red_led.toggle()
 
-            # Check if line was found in previous iteration
+            # Filtered list of lines within theta limits
+            lines_present.append(line)
+
+            # Skip to next line if line is below/above limits
             for iteration, existing_line in self._known_lines:
                delta_array = ulab.array(line.line()) - ulab.array(existing_line.line())
                # Compare each coordinate, x1,x2,y1,y2
@@ -166,22 +152,16 @@ class VeloTimer():
                       # Coordinate delta too high, not a match
                       break
                else:
-                   # Line matches previous line, update coordinates
+                   # Compare each coordinate, x1,x2,y1,y2
                    self._known_lines.remove([iteration, existing_line])
                    self._known_lines.append([0,line])
                    # No need to process more lines
                    break
-
-               # Purge old line mactches
-               #self.green_led.off()
-               #print("Line removed")
-               #self._lines.remove(existing_line)
             else:
                 # If no matching line was found it must be new
                 self.green_led.on()
                 print("New line found")
                 self._known_lines.append([0, line])
-
         for place, iteration_line in enumerate(self._known_lines):
             if iteration_line[1] not in lines_present:
                 if iteration_line[0] >= self.frames_before_line_purge:
@@ -189,13 +169,11 @@ class VeloTimer():
                      print("Purged line")
                 else:
                     iteration_line[0] += 1
-
         if not self._known_lines:
             self.green_led.off()
-
         return lines_present
 
 timer = VeloTimer(draw_stats=True, draw_lines=True)
 while(True):
-    timer.snapshot() # Take a picture store
-    timer.find_lines()
+    timer.snapshot() # Take a picture
+    timer.find_lines() # Process the picture
