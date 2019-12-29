c50d6266bc8a89b56d4f01e463a90d5d08ce1d0b
Antonia Stevens <a@antevens.com>
2019-12-29 17:19:18 -0500

    Character updating fixed

---
+++ hud/sensors/sensors.ino
@@ -0,0 +1,186 @@
+#include "hyperdisplay_conf.h"
+#include <HyperDisplay_UG2856KLBAG01.h>
+#include <ArduinoBLE.h>
+#include <Arduino_LSM9DS1.h>
+#include <Arduino_LPS22HB.h>
+#include <Arduino_HTS221.h>
+#include <Arduino_APDS9960.h>
+
+// add voice control
+// add bluetooth pairing
+
+// The relative humidity and temperature sensor is a HTS221
+// The barometer sensor is a LPS22HB
+// The Gesture sensor is a APDS9960
+// The digital microphone is a MP34DT05
+// The IMU is a LSM9DS1
+
+//////////////////////////////
+//    Pinout and Hardware   //
+//////////////////////////////
+#define SERIAL_PORT Serial
+#define SPI_PORT SPI
+#define CS_PIN 9
+#define DC_PIN 10
+// END USER SETUP
+
+// Set up Transparent Organic LED Display and SPI communications
+UG2856KLBAG01_SPI TOLED;
+
+// Create Windows
+wind_info_t windowZero, windowOne, windowTwo, windowThree, windowFour, windowFive;  // Create some window objects
+
+void setup() {
+  // Start serial communications
+  Serial.begin(9600);
+  Serial.println(F("Dromedary: Transparent Graphical OLED HUD"));
+
+  // Start TOLED/SPI communications
+  SPI_PORT.begin();
+  TOLED.begin(CS_PIN, DC_PIN, SPI_PORT);
+
+  // Start sensor communications
+  IMU.begin();
+  BARO.begin();
+  HTS.begin();
+  APDS.begin();
+
+  APDS.setGestureSensitivity(100);
+  //APDS.setLEDBoost(); 0/1/2/3
+
+  TOLED.setWindowDefaults(&windowZero);
+  TOLED.setWindowDefaults(&windowOne);
+  TOLED.setWindowDefaults(&windowTwo);
+  TOLED.setWindowDefaults(&windowThree);
+  TOLED.setWindowDefaults(&windowFour);
+  TOLED.setWindowDefaults(&windowFive);
+
+  TOLED.setWindowColorSet(&windowZero);
+  TOLED.setWindowColorSet(&windowOne);
+  TOLED.setWindowColorSet(&windowTwo);
+  TOLED.setWindowColorSet(&windowThree);
+  TOLED.setWindowColorSet(&windowFour);
+  TOLED.setWindowColorSet(&windowFive);
+
+  windowZero.xMin = 0;
+  windowZero.yMin = 0;
+  windowZero.xMax = 127;
+  windowZero.yMax = 9;
+  windowZero.clearCharacterArea = true;
+
+  windowOne.xMin = 0;
+  windowOne.yMin = 10;
+  windowOne.xMax = 127;
+  windowOne.yMax = 19;
+  windowOne.clearCharacterArea = true;
+
+  windowTwo.xMin = 0;
+  windowTwo.yMin = 20;
+  windowTwo.xMax = 127;
+  windowTwo.yMax = 29;
+  windowTwo.clearCharacterArea = true;
+
+  windowThree.xMin = 0;
+  windowThree.yMin = 30;
+  windowThree.xMax = 127;
+  windowThree.yMax = 39;
+  windowThree.clearCharacterArea = true;
+
+  windowFour.xMin = 0;
+  windowFour.yMin = 40;
+  windowFour.xMax = 127;
+  windowFour.yMax = 49;
+  windowFour.clearCharacterArea = true;
+
+  windowFive.xMin = 0;
+  windowFive.yMin = 50;
+  windowFive.xMax = 127;
+  windowFive.yMax = 59;
+  windowFive.clearCharacterArea = true;
+
+  //TOLED.setContrastControl(255)
+  TOLED.pCurrentWindow = &windowZero;
+  TOLED.println("Dromedary");
+  //TOLED.setTextCursor(0,10);
+  TOLED.resetTextCursor();
+  TOLED.pCurrentWindow = &windowOne;
+  TOLED.println("starting ...");
+  TOLED.resetTextCursor();
+  TOLED.pCurrentWindow = &windowTwo;
+  //TOLED.setTextCursor(0,20);
+  TOLED.println("finding sensors ...");
+  TOLED.resetTextCursor();
+  //delay(3000);
+  //TOLED.windowSet();
+  TOLED.windowClear();
+}
+
+float xa, ya, za;
+float xg, yg, zg;
+void loop() {
+  IMU.readAcceleration(xa, ya, za);
+  IMU.readGyroscope(xg, yg, zg);
+  float pressure = BARO.readPressure();
+  float temperature = HTS.readTemperature();
+  float humidity = HTS.readHumidity();
+  /*
+  APDS9960 - Gesture Sensor
+
+  Gesture directions are as follows:
+  - UP:    from USB connector towards antenna
+  - DOWN:  from antenna towards USB connector
+  - LEFT:  from analog pins side towards digital pins side
+  - RIGHT: from digital pins side towards analog pins side
+  */
+
+  String accelData = "X " + String(xa, 1) + " Y " + String(ya, 1) + " Z " + String(za, 1);
+  String gyroData = "X " + String(xg, 1) + " Y " + String(yg, 1) + " Z " + String(zg, 1);
+  TOLED.pCurrentWindow = &windowZero;
+  TOLED.println(accelData);
+  TOLED.resetTextCursor();
+  TOLED.pCurrentWindow = &windowOne;
+  TOLED.println(gyroData);
+  TOLED.resetTextCursor();
+  TOLED.pCurrentWindow = &windowTwo;
+  TOLED.println(String(pressure, 2) + " kPa");
+  TOLED.resetTextCursor();
+  TOLED.pCurrentWindow = &windowThree;
+  TOLED.println(String(temperature, 2) + " C");
+  TOLED.resetTextCursor();
+  TOLED.pCurrentWindow = &windowFour;
+  TOLED.println(String(humidity, 2) + " %");
+  TOLED.resetTextCursor();
+  TOLED.pCurrentWindow = &windowFive;
+  if (APDS.gestureAvailable()) {
+    // Detected a guesture
+    TOLED.println("Gesture: " + String(APDS.readGesture()));
+    TOLED.resetTextCursor();
+  } else {
+    TOLED.println("Proximity: " + String(APDS.readProximity()));
+    TOLED.resetTextCursor();
+  }
+
+  //TOLED.rectangle(0, 0, 100, 100,true,(color_t)&color);        //font "color"
+  // First let's draw a line on the default window (which is equivalent to the whole screen)
+  //TOLED.lineSet(0, 0, 40, 60);
+  //IMU.readAcceleration(x, y, z);
+  //TOLED.setContrastControl(0);
+  //Serial.println("stuff");
+  //TOLED.pCurrentWindow = &pairingWindow;
+  //TOLED.clearDisplay();
+  //TOLED.pCurrentWindow = &windowZero;
+  //TOLED.windowClear();
+  //TOLED.println("752W");
+  //TOLED.resetTextCursor();
+  //TOLED.setTextCursor(0,10);
+  //TOLED.println(x);
+  //TOLED.println(y);
+  //TOLED.println(z);
+  //TOLED.pCurrentWindow = &defaultWindow;
+  //TOLED.setTextCursor(0,20);
+  //TOLED.pCurrentWindow = &windowOne;
+  //TOLED.windowClear();
+  //TOLED.println("62.3km/h");
+  //TOLED.show();
+  //TOLED.resetTextCursor();
+}
--- hud/text/text.ino
+++ hud/text/text.ino
@@ -1,11 +1,16 @@
 #include "hyperdisplay_custom_conf.h"
 #include "HyperDisplay_UG2856KLBAG01.h"
 #include <ArduinoBLE.h>
+#include <Arduino_LSM9DS1.h>
 
 // add voice control
 // add bluetooth pairing
-uint8_t defaultScreenMem[128*56];    // Reserve 128*56 bytes worth of memory
-uint8_t pairingScreenMem[128*56];    // Reserve 128*56 bytes worth of memory
+
+// The relative humidity and temperature sensor is a HTS221
+// The barometer sensor is a LPS22HB
+// The Gesture sensor is a APDS9960
+// The digital microphone is a MP34DT05
+// The IMU is a LSM9DS1 
 
 //////////////////////////////
 //    Pinout and Hardware   //
@@ -23,79 +28,92 @@ uint8_t noColor = 0x00;
 // Set up Transparent Organic LED Display and SPI communications
 UG2856KLBAG01_SPI TOLED;
 
-// Set up windows
-wind_info_t defaultWindow, pairingWindow;
-  
-void setup() {
-  // Configure windows
-  TOLED.setWindowDefaults(&defaultWindow);
-  TOLED.setWindowDefaults(&pairingWindow);
+// Create Windows
+wind_info_t windowZero, windowOne, windowTwo, windowThree, windowFour, windowFive;  // Create some window objects
 
-  pairingWindow.xMin = 110;
-  pairingWindow.yMin = 10;
-  pairingWindow.xMax = 54;
-  pairingWindow.yMax = 54;
-  defaultWindow.xMin = 0;
-  defaultWindow.yMin = 0;
-  defaultWindow.xMax = 128;
-  defaultWindow.yMax = 56;
+void setup() {
+  // Start serial communications
+  Serial.begin(9600);
+  Serial.println(F("Dromedary: Transparent Graphical OLED HUD"));
   
   // Start TOLED/SPI communications
   SPI_PORT.begin();
   TOLED.begin(CS_PIN, DC_PIN, SPI_PORT);
-  //TOLED.setWindowColorClear()
-  //TOLED.setWindowColorSet()
-  
-  TOLED.setCurrentWindowColorSequence((color_t)&color);
-  TOLED.setCurrentWindowMemory((color_t)defaultScreenMem, 128*56);
-  
-  TOLED.setWindowColorSequence(&pairingWindow, (color_t)&color);
-  TOLED.setWindowColorSequence(&defaultWindow, (color_t)&color);
 
-  TOLED.setWindowMemory(&pairingWindow, (color_t)pairingScreenMem, 128*56);
-  TOLED.setWindowMemory(&defaultWindow, (color_t)pairingScreenMem, 128*56);
+  // Start sensor communications
+  IMU.begin();
 
-  //TOLED.setContrastControl(0)
+  TOLED.setWindowDefaults(&windowZero);
+  TOLED.setWindowDefaults(&windowOne);
+  TOLED.setWindowDefaults(&windowTwo);
+  TOLED.setWindowDefaults(&windowThree);
+  TOLED.setWindowDefaults(&windowFour);
+  TOLED.setWindowDefaults(&windowFive);
+   
+  TOLED.setWindowColorSequence(&windowZero, (color_t)&color);
+  TOLED.setWindowColorSequence(&windowOne, (color_t)&color);
+  TOLED.setWindowColorSequence(&windowTwo, (color_t)&color);
+  TOLED.setWindowColorSequence(&windowThree, (color_t)&color);
+  TOLED.setWindowColorSequence(&windowFour, (color_t)&color);
+  TOLED.setWindowColorSequence(&windowFive, (color_t)&color);
+  
+  windowZero.xMin = 0;
+  windowZero.yMin = 0;
+  windowZero.xMax = 127;
+  windowZero.yMax = 9;
+  
+  windowOne.xMin = 0;
+  windowOne.yMin = 10;
+  windowOne.xMax = 127;
+  windowOne.yMax = 19;
+    
+  windowTwo.xMin = 0;
+  windowTwo.yMin = 20;
+  windowTwo.xMax = 127;
+  windowTwo.yMax = 29;
+  
   //TOLED.setContrastControl(255)
-  
+  TOLED.pCurrentWindow = &windowZero;
   TOLED.println("Pairing");
-  TOLED.pCurrentWindow = &pairingWindow;
-  TOLED.buffer(&pairingWindow);
-  TOLED.show(&pairingWindow);
-  //TOLED.fillWindow((color_t)color);
-  TOLED.setTextCursor(0,10);
+  //TOLED.setTextCursor(0,10);
+  TOLED.resetTextCursor();
+  TOLED.pCurrentWindow = &windowOne;
   TOLED.println("Doing");
-  delay(5000);
-  TOLED.show(&pairingWindow);
-  delay(5000);
-  TOLED.pCurrentWindow = &defaultWindow;
-  TOLED.show(&defaultWindow);
-  //TOLED.buffer();
-  TOLED.setTextCursor(0,20);
+  TOLED.resetTextCursor();
+  TOLED.pCurrentWindow = &windowTwo;
+  //TOLED.setTextCursor(0,20);
   TOLED.println("Jumping");
-  delay(1000);
-  //TOLED.show();
-  delay(1000);
-  
-  // Start serial communications
-  Serial.begin(9600);
-  Serial.println(F("Dromedary: Transparent Graphical OLED HUD"));
+  TOLED.resetTextCursor();
+  //delay(3000);
+  //TOLED.windowSet();
+  delay(3000);
+  TOLED.windowClear();
+
 }
 
+float x, y, z;
 void loop() {
   //TOLED.rectangle(0, 0, 100, 100,true,(color_t)&color);        //font "color"
   // First let's draw a line on the default window (which is equivalent to the whole screen)
   //TOLED.lineSet(0, 0, 40, 60);
+  //IMU.readAcceleration(x, y, z);
+  //TOLED.setContrastControl(0);
   Serial.println("stuff");
   //TOLED.pCurrentWindow = &pairingWindow;
-  TOLED.clearDisplay();
+  //TOLED.clearDisplay();
+  TOLED.pCurrentWindow = &windowZero;
+  TOLED.windowClear();
   TOLED.println("752W");
-  TOLED.setTextCursor(0,10);
-  TOLED.println("11.8s");
-  TOLED.setTextCursor(0,20);
-  TOLED.println("62.3km/h");
+  TOLED.resetTextCursor();
+  //TOLED.setTextCursor(0,10);
+  //TOLED.println(x);
+  //TOLED.println(y);
+  //TOLED.println(z);
   //TOLED.pCurrentWindow = &defaultWindow;
+  //TOLED.setTextCursor(0,20);
+  TOLED.pCurrentWindow = &windowOne;
+  TOLED.windowClear();
+  TOLED.println("62.3km/h");
   //TOLED.show();
-  delay(5000);
   TOLED.resetTextCursor();
 }
